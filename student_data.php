31d2f45e2b8ce7e61aaff199f9359489b7ffb339
Karunagaran <111301820+Info-God@users.noreply.github.com>
2023-06-21 11:54:38 +0530

    Delete student_data.php

--- student_data.php
+++
@@ -1,8 +0,0 @@
-<?php 
-class student_data extends CI_Model{
-    function storeData($studentData){
-        $this->load->database();
-        return $this->db->insert("staff",$studentData);
-    }
-
-}
